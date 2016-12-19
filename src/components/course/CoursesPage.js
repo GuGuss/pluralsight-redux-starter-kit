6b20fe579faf3b3521c2e29d37e13f2e0c06ed6e
GuGuss <augustin@commerceguys.com>
2016-12-19 18:01:42 +0100

    Initial App Structure.

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+
+// Is not a stateless component (const) because of some current Hot Reloading limitations.
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
